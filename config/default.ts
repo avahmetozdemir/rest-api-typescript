9a226306087a29fee813428727d228ebd7d22b02
avahmetozdemir <avamertozdemir@gmail.com>
2022-12-20 12:17:46 +0300

    04- user model is completed

--- config/default.ts
+++ config/default.ts
@@ -2,4 +2,5 @@ export default {
   port: 1337,
   dbUri:
     "mongodb+srv://avahmetozdemir:Password8067.@cluster0.fifnsft.mongodb.net/rest-api-tutorial?retryWrites=true&w=majority",
+  saltWorkFactor: 10,
 };
